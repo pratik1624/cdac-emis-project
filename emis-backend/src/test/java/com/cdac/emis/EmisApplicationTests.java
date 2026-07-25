ddbf6309c4712bf17fdcd7cf20b47be35f2f7570
AC-96376-pratik <pratikkhobragade1624@gmail.com>
2026-07-25 19:39:00 +0530

    backend project init

---
+++ emis-backend/src/test/java/com/cdac/emis/EmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.emis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
