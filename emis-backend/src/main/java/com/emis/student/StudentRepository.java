8ce897606546a0b1356d97cfac3f3d7efd9503a8
= <pratikkhobragade1624@gmail.com>
2026-07-28 22:52:57 +0530

    student-repo fix

--- emis-backend/src/main/java/com/emis/student/StudentRepository.java
+++ emis-backend/src/main/java/com/emis/student/StudentRepository.java
@@ -5,10 +5,12 @@ import com.emis.student.dto.StudentProfileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByDepartmentAndSemester(Department department , Integer semester);
+    Optional<Student> findByUserDetailsEmail(String email);
 }
 
 
