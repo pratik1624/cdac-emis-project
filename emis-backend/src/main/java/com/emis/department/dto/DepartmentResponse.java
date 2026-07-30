e9f09df8663cd0924854d85fd92de529892f300b
bhagyesh akhare <bhagyeshakhare@gamil.com>
2026-07-30 18:22:29 +0530

    backend department

---
+++ emis-backend/src/main/java/com/emis/department/dto/DepartmentResponse.java
@@ -0,0 +1,16 @@
+package com.emis.department.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartmentResponse {
+
+    private Long id;
+
+    private String departmentCode;
+
+    private String deptName;
+
+}
