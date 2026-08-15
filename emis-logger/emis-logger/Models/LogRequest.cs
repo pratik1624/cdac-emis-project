ff68659cfc09c57a45dfbc30b95fb3a71e437328
pratik khobragade <pratikkhobragade1624@gmail.com>
2026-08-15 23:02:51 +0530

    microservice

--- emis-logger/emis-logger/Models/LogRequest.cs
+++ emis-logger/emis-logger/Models/LogRequest.cs
@@ -1,6 +1,13 @@
-﻿namespace emis_logger.Models
+﻿namespace LoggerService.Models
 {
     public class LogRequest
     {
+        public string ServiceName { get; set; }
+
+        public string Level { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimeStamp { get; set; }
     }
 }
